48d2aee669cdf0cdeb6b3a21bef6882fb3709b0a
Ilham <ilham@Ilhams-MacBook-Air.local>
2024-01-26 13:52:23 +0400

    task3

---
+++ task3.java
@@ -0,0 +1,19 @@
+
+public class task3 {
+    
+
+public static void main(String[] args) {
+   
+    int firstNumber = 5;
+    long secondNumber = 10000000000L; 
+    float thirdNumber = 3.14f; 
+
+    // Data type of the sum variable is float as it can hold the sum of int, long, and float.
+    float sum = firstNumber + secondNumber + thirdNumber;
+
+    
+    System.out.println("Sum: " + sum);
+
+    
+}
+}
