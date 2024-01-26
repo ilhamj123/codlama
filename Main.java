3f6c989d90d60b0a50761c651ac418a9dc788c1c
Ilham <ilham@Ilhams-MacBook-Air.local>
2024-01-26 11:01:39 +0400

    task1

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+ System.out.print("Enter your name: ");
+// Read the user's input
+        String Name = scanner.nextLine();
+// Print the name
+    System.out.println("Hello, " + Name);
+scanner.close();
+    }
+}
