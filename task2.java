0ddcff65f84a582d8107cfab4a0ef7826a91b80f
ilhamj123 <151451265+ilhamj123@users.noreply.github.com>
2024-01-26 13:30:32 +0400

    it is new project

---
+++ task2.java
@@ -0,0 +1,17 @@
+package week01;
+
+public class task2 {
+    public static void main(String[] args) {
+        
+        int num1 = 5;
+        int num2 = 10;
+        int num3 = 7;
+
+        
+        int sum = num1 + num2 + num3;
+
+       
+        System.out.println("Sum of the numbers: " + sum);
+    }
+}
+
