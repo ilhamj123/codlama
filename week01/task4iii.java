091130f401dbd3be38afb50a749989db9c0e82f8
Ilham <ilham@Ilhams-MacBook-Air.local>
2024-01-27 18:39:56 +0400

    task4 iii

---
+++ week01/task4iii.java
@@ -0,0 +1,10 @@
+package week01;
+
+public class task4iii {
+    public static void main(String[] args) {
+        
+        double result = (1.0 / 5) * (2.0 / 3) - (3.0 / 10);
+         System.out.printf("Result: " + result);
+    }
+}
+
