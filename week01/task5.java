971f690f57fdd7f28f5c54008ce3ed545f0364a8
Ilham <ilham@Ilhams-MacBook-Air.local>
2024-01-27 18:48:45 +0400

    task 5 solution

---
+++ week01/task5.java
@@ -0,0 +1,23 @@
+package week01;
+
+public class task5 {
+    public class Main {
+        public static void main(String[] args) {
+            // Part a:
+            int intValue = 42;
+            
+            // Part b: 
+            System.out.println("Integer value (decimal): " + intValue);
+            System.out.println("Integer value (hexadecimal): " + Integer.toHexString(intValue));
+            System.out.println("Integer value (octal): " + Integer.toOctalString(intValue));
+            
+            // Part c: 
+            float floatValue = 3.14f;
+            
+            // Part d: 
+            System.out.println("Float value (decimal floating-point): " + floatValue);
+            System.out.println("Float value (scientific notation): " + String.format("%e", floatValue));
+        }
+    }
+    
+}
