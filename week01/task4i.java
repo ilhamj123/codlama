2f7bc5f21adb7cd2d014d81299cc2be0bc97129f
Ilham <ilham@Ilhams-MacBook-Air.local>
2024-01-27 18:30:25 +0400

    task4 solution

---
+++ week01/task4i.java
@@ -0,0 +1,12 @@
+package week01;
+
+public class task4i {
+    public static void main(String[] args) {
+        
+        double result = (1.0/5) * (2.0/3) - (3.0/10);
+        
+        
+        System.out.println("Result: " + result);
+    }
+}
+
